545b1f7c2fccf684e14fed8238450b858d77c8a1
FliegendeWurst <2012gdwu+github@posteo.de>
2021-03-28 14:27:37 +0200

    new: display date

--- src/bin/new.rs
+++ src/bin/new.rs
@@ -1,5 +1,6 @@
 use ascii_table::{Align, AsciiTable, Column};
-use mailparse::MailHeaderMap;
+use chrono::{Local, NaiveDateTime, TimeZone};
+use mailparse::{MailHeaderMap, dateparse};
 
 use inboxid::*;
 
@@ -11,19 +12,26 @@ fn show_listing(mailbox: &str) -> Result<()> {
 	let maildir = get_maildir(mailbox)?;
 
 	let mut rows = Vec::new();
-	for mail in maildir.list_new_sorted(Box::new(|name| {
+	for mut mail in maildir.list_new_sorted(Box::new(|name| {
 		// sort by UID
 		name.splitn(2, '_').nth(1).map(|x| x.parse().unwrap_or(0)).unwrap_or(0)
 	})) {
-		match mail {
-		    Ok(mut mail) => {
-				let mail = mail.parsed()?;
+		match mail.as_mut().map(|x| x.parsed()) {
+		    Ok(Ok(mail)) => {
 				let headers = mail.get_headers();
 				let from = headers.get_all_values("From").join(" ");
 				let subj = headers.get_all_values("Subject").join(" ");
-				rows.push(vec![from, subj]);
+				let date = headers.get_all_values("Date").join(" ");
+				let date = dateparse(&date).map(|x| {
+					let dt = Local.from_utc_datetime(&NaiveDateTime::from_timestamp(x, 0));
+					dt.format("%Y-%m-%d %H:%M").to_string()
+				}).unwrap_or(date);
+				rows.push(vec![from, subj, date]);
 			}
-		    Err(e) => {
+		    Ok(Err(e)) => {
+				println!("error parsing mail: {:?}", e);
+			}
+			Err(e) => {
 				println!("error: {:?}", e);
 			}
 		}
@@ -35,6 +43,7 @@ fn show_listing(mailbox: &str) -> Result<()> {
     for (i, &(header, align)) in [
         ("From", Align::Left),
         ("Subject", Align::Left),
+		("Date", Align::Left),
     ].iter().enumerate() {
             let mut column = Column::default();
             column.header = header.to_owned();
